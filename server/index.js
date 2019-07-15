45645497512fd209998c059e07c32b369f3a8f46
Igor Imperiali <ifernandes@estantevirtual.com.br>
2019-07-15 18:06:26 -0300

    Client and server ready to deploy

--- server/index.js
+++ server/index.js
@@ -12,6 +12,15 @@ const posts = require('./routes/api/posts')
 
 app.use('/api/posts', posts)
 
+// handle Production
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(__dirname + '/public/'))
+
+  // handle SPA
+  app.get(/.*/, (req, res) => res.sendfile(__dirname + '/public/index.html'))
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
